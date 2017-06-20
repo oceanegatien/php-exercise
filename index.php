f7dc50c8b96c37a42411c7a9652607de3d0083b1
oceane <oceane.gatien.cef@hotmail.fr>
2017-06-20 12:19:49 +0200

    Exercice 1/2/3

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
 <?php 
-	echo "coucou";
+	// les boucles 
+
+// Exercice 1
+	// $ex1 = 0;
+	// for ($i=0; $i < 10 ; $i++) { 
+	// 	echo $ex1;
+	// 	$ex1++;
+	// }
+
+
+//Exercice 2
+	// $ex21 = 0;
+	// $ex22 = 2;
+
+	
+	// for ($i=0; $i <= 20 ; $i++) { 
+	// 	$ex23 = $ex21 * $ex22;
+	// 	echo $ex23;
+	// 	$ex21++;
+	// }
+
+// Exercice 3
+	$ex31 = 100;
+	$ex32 = 79;
+
+for ($i=100; $i > 10; $i--) { 
+	$ex33 = $ex31 * $ex32;
+	echo $ex33;
+	$ex31--;
+}
+
+
+
 ?>
