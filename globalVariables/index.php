c047dd9f404a368347f108c084c3ebc937bc93b4
oceane <oceane.gatien.cef@hotmail.fr>
2017-06-23 15:24:49 +0200

    Global variable - Exercice 3 - 4 - 5

--- globalVariables/index.php
+++ globalVariables/index.php
@@ -6,22 +6,45 @@
 <body>
 
 	<?php 
-$user = $_SERVER['HTTP_USER_AGENT'];
-$ip = $_SERVER["REMOTE_ADDR"];
-$nom = gethostname();
-$serveur = $_SERVER['SERVER_NAME'];
-echo "User agent: ".$user."</br>";
-echo "Nom : ".$nom."</br>";
-echo "IP: ".$ip."</br>";
-echo "Nom serveur: ".$serveur."</br>";
+		// Exercice 1 - 2
+		// $user = $_SERVER['HTTP_USER_AGENT'];
+		// $ip = $_SERVER["REMOTE_ADDR"];
+		// $nom = gethostname();
+		// $serveur = $_SERVER['SERVER_NAME'];
+		// echo "User agent: ".$user."</br>";
+		// echo "Nom : ".$nom."</br>";
+		// echo "IP: ".$ip."</br>";
+		// echo "Nom serveur: ".$serveur."</br>";
 
-session_start();
+		// session_start();
 
-$_SESSION['nom'] = 'Gatien';
-$_SESSION['prenom'] = $nom;
-$_SESSION['age'] = 27;
+		// $_SESSION['nom'] = 'Gatien';
+		// $_SESSION['prenom'] = $nom;
+		// $_SESSION['age'] = 27;
+
+	?>
+	<form action="mapage.php" method="post">
+		<label for="pseudo">Pseudo</label>
+		<input id="pseudo" type="text" name="pseudo"/>
+
+		<label for="password">Mot de passe</label>
+		<input id="password" type="password" name="password"/>
+
+		<button type="submit">OK</button>
+	</form>
+	<?php 
+		// Exercice 3 - 4 - 5
+
+		$pseudo = $_POST['pseudo'];
+		$password = $_POST['password'];
+		
+		setcookie('pseudo', $pseudo, time() + 100000); 
+		setcookie('password', $password, time() + 100000);
+		
+
+
 	?>
 
-<a href="mapage.php">Lien vers mapage.php</a>
+	<a href="mapage.php">Lien vers mon profil</a>
 </body>
 </html>
--- globalVariables/mapage.php
+++ globalVariables/mapage.php
@@ -1,15 +1,18 @@
 <?php 
-session_start();
+// session_start();
 
 
 
-$nom = $_SESSION['nom'];
-$prenom = $_SESSION['prenom'];
-$age = $_SESSION['age'];
-echo "Bonjour, ".$nom." ".$prenom."</br>";
-echo $age;
+// $nom = $_SESSION['nom'];
+// $prenom = $_SESSION['prenom'];
+// $age = $_SESSION['age'];
+// echo "Bonjour, ".$nom." ".$prenom."</br>";
+// echo $age;
 
+// Exercice 4 - 5
+$pseudo = $_COOKIE['pseudo'];
+$password = $_COOKIE['password'];
 
-
-
+echo "Re-bonjour, ".$pseudo."</br>";
+echo "MDP: ".$password;
 ?>
