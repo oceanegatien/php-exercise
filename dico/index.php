0fcd5817b39f9ce7acc0ce7d31033db79302a0d2
oceane <oceane.gatien.cef@hotmail.fr>
2017-06-26 12:08:52 +0200

    fin exercices dico

--- dico/index.php
+++ dico/index.php
@@ -10,32 +10,44 @@
 		$dico = explode("\n", $string);
 
 
-		$nb = count($dico);
-		echo "Le dictionnaire contient ".$nb." mots.</br>";
+		// $nb = count($dico);
+		// echo "Le dictionnaire contient ".$nb." mots.</br>";
 		
-		$array15 = array();
-		foreach ($dico as $value) {
-			$strlen = strlen($value) === 15;
-			if ($strlen === true) {
-				$array[] = $value;
-			}
-		}
-			$count = count($array);
-			echo "Le dictionnaire contient ".$count." mots de 15 lettres.</br>";
+		// $array15 = array();
+		// foreach ($dico as $value) {
+		// 	$strlen = strlen($value) === 15;
+		// 	if ($strlen === true) {
+		// 		$array[] = $value;
+		// 	}
+		// }
+		// 	$count = count($array);
+		// 	echo "Le dictionnaire contient ".$count." mots de 15 lettres.</br>";
 
-		$arrayW = array();
+		// $arrayW = array();
+		// foreach ($dico as $value) {
+		// 	$strSplit = str_split($value);
+
+		// 	foreach ($strSplit as $value2) {
+		// 		if ($value2 === "w") {
+		// 			$arrayW[] = $value;
+		// 		}
+		// 	}
+			
+		// }
+		// 	$count2 = count($arrayW);
+		// 	echo "Le dictionnaire contient ".$count2." mots avec un W.</br>";
+
+		$arrayQ = array();
 		foreach ($dico as $value) {
 			$strSplit = str_split($value);
-
-			foreach ($strSplit as $value2) {
-				if ($value2 === "w") {
-					$arrayW[] = $value;
-				}
+			$end = array_pop($strSplit);
+			if ($end === "q") {
+				$arrayQ[] = $value;
 			}
-			
 		}
-			$count2 = count($arrayW);
-			echo "Le dictionnaire contient ".$count2." mots avec un W.</br>";;
+		$count3 = count($arrayQ);
+		echo "Le dictionnaire contient ".$count3." mots qui finnissent par un Q.</br>";
+
 			
 		
 
